641fcf21b333289259d3f7d86260eb508e1d5912
jardimdanificado <jardeltorreslima01@gmail.com>
2024-04-14 07:50:48 -0300

    updated version number

--- src/br.lua
+++ src/br.lua
@@ -26,7 +26,7 @@ local br =
     vm = 
     {
         -- version
-        version = "0.2.6r",
+        version = "0.2.6s",
         -- source and outputs
         source = "",
         outputpath = "",
