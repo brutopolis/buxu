4996b0be52efeb3a2e9895ab93d3c510f3c38ece
jardimdanificado <jardeltorreslima01@gmail.com>
2024-10-19 03:40:36 -0300

    new file lib/example.c

---
+++ lib/example.c
@@ -0,0 +1,24 @@
+#include "bruter.h"
+
+/*
+    this lib is an example of how to create a library for the bruter vm
+*/
+
+#ifndef ARDUINO
+
+Int example_function(VirtualMachine *vm, IntList *args) // example function
+{
+    printf("Hello, World!\n");
+    return -1;
+}
+
+void init_example(VirtualMachine *vm) // example lib initializer
+{
+    registerBuiltin(vm, "example", example_function); // register example function
+    registerNumber(vm, "example.number", 42); // register example number
+    registerString(vm, "example.string", "Hello, World!"); // register example string
+    registerFunction(vm, "example.function", "print(\"Hello, World!\")"); // register example function
+    registerList(vm, "example.list"); // register example list
+}
+
+#endif
