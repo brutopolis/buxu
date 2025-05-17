8f180c7fc76fa4d12c0fe35c783270a5e2d1ba6f
jardimdanificado <jardeltorreslima01@gmail.com>
2025-05-17 04:44:51 -0300

    cli.c and buxu.h merged

--- src/buxu.c
+++ src/buxu.c
@@ -3,9 +3,25 @@
 // there are some tweaks that try be compatible with mac and windows, totally untested;
 
 // buxu header
-#include "buxu.h"
+#include "br.h"
 
-#include <dlfcn.h> // dynamic library loading
+// dynamic library loading
+#include <dlfcn.h> 
+
+#define BUXU_VERSION "0.1.7"
+
+// emoticons
+#define EMOTICON_DEFAULT "[=°-°=]"
+#define EMOTICON_IDLE "[=° °=]"
+#define EMOTICON_WARNING "[=°~°=]"
+#define EMOTICON_ERROR "[=°x°=]"
+#define EMOTICON_CONFUSED "[=º?°=]"
+
+// define a macro to printf but it always starts with [=°-°=] and ends with a newline
+#define buxu_print(emoticon, ...) printf(emoticon ": "); printf(__VA_ARGS__); printf("\n")
+#define buxu_warn(...) printf(EMOTICON_WARNING ": warning: "); printf(__VA_ARGS__); printf("\n")
+#define buxu_error(...) printf(EMOTICON_ERROR ": error: "); printf(__VA_ARGS__); printf("\n")
+#define buxu_info(...) printf(EMOTICON_DEFAULT ": info: "); printf(__VA_ARGS__); printf("\n")
 
 List *args;
 List* libs;
